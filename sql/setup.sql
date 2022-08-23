219e9e0b4ac6caf1f7df7a7a5ff5167288ae5c17
Allison Ause <allison.ause@gmail.com>
2022-08-22 17:48:26 -0700

    setup sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users, tasks;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE tasks (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT,
+  content VARCHAR NOT NULL,
+  bought BOOLEAN NOT NULL DEFAULT(false),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
