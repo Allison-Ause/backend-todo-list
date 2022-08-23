4ef38a88a8bfc7566afd959e2cf42194752d12db
Allison Ause <allison.ause@gmail.com>
2022-08-23 10:01:32 -0700

    built out UserService model

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const testUser = {
+  firstName: 'Kylo',
+  lastName: 'Ren',
+  email: 'darkside@redemption.com',
+  password: 'iloverey',
+};
+
+describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#POST /users signs up a new user if none exists', async () => {
+    const res = request(app).post('/api/v1/users').send(testUser);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...testUser,
+    });
   });
   afterAll(() => {
     pool.end();
