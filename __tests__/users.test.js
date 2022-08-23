ca4619da3baae377cae8b117b7c914c969a4eacd
Allison Ause <allison.ause@gmail.com>
2022-08-23 15:42:27 -0700

    functioning route for /me

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -10,11 +10,18 @@ const testUser = {
   password: 'iloverey',
 };
 
+const existingUser = {
+  firstName: 'Allison',
+  lastName: 'Ause',
+  email: 'allison@works.com',
+  password: 'fakePasswordHash',
+};
+
 describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it.skip('#POST /users signs up & in a new user if none exists', async () => {
+  it('#POST /users signs up & in a new user if none exists', async () => {
     const res = await request(app).post('/api/v1/users').send(testUser);
 
     expect(res.status).toBe(200);
@@ -23,22 +30,29 @@ describe('user routes', () => {
     );
   });
   it('#POST /users signs in user if exists', async () => {
-    const existingUser = {
-      firstName: 'Allison',
-      lastName: 'Ause',
-      email: 'allison@works.com',
-      password: 'fakePasswordHash',
-    };
     const agent = request.agent(app);
-    const signIn = await agent.post('/api/v1/users').send(existingUser);
-    console.log('SignedIN', signIn);
-    const res = await agent.post('/api/v1/users/sessions').send(existingUser);
+    await agent.post('/api/v1/users').send(existingUser);
 
+    const res = await agent.post('/api/v1/users/sessions').send(existingUser);
     expect(res.status).toBe(200);
     expect(res.body.message).toBe(
       'Welcome to the Rodeo! You are all signed in.'
     );
   });
+  it('#GET /me returns user info if logged in', async () => {
+    const agent = request.agent(app);
+    await agent.post('/api/v1/users').send(existingUser);
+
+    const res = await agent.get('/api/v1/users/me');
+    console.log('res.body from /me', res.body);
+    expect(res.status).toBe(200);
+    expect(res.body.email).toEqual('allison@works.com');
+  });
+  it('#GET /me returns 401 if not logged in', async () => {
+    const res = await request(app).get('/api/v1/users/me');
+    expect(res.status).toBe(401);
+  });
+
   afterAll(() => {
     pool.end();
   });
